f1c227b6f1a3325831f56ccdc93fb171addac7cc
Ludielly <ludysilveira02@gmail.com>
2023-10-10 13:40:44 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// import Link from "next/link";
+// import Footer from "../src/components/patterns/Footer";
+
+// function Title({children, as}) {
+//     const Tag = as;
+//     return (
+//         <>
+//             <Tag>{children}</Tag>
+//             <style jsx>{`
+//                 ${Tag} {
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+//     )
+// }
+
+// export default function HomePage() {
+//     return (
+//         <>
+//             <Title as="h1">Alura Cases - HomePage</Title>
+//             <Link href="/faq">
+//                 Ir para FAQ
+//             </Link>
+//             <Footer />
+//         </>
+//     )
+// }
